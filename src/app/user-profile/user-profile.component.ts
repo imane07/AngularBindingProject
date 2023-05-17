eb955d191feda85ff126ffdfee601d8893e67c20
Imane EL <imane.elkhandouki@gmail.com>
2023-05-17 20:40:08 +0200

    Ajout du projet

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent {
+
+  isHidden: boolean = false;
+
+  user = {
+    name: 'Doe',
+    firstName: 'John',
+    age: 25,
+    quote: '',
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+
+  masquerAge() {
+    this.isHidden = true;
+  }
+
+}
